c248452002620e0311222a8d739ffbee33d9c567
S9-User <propoziti@yandex.ru>
2024-11-17 18:08:11 +0300

    Игорёха - S9-User - Task1 Попытка номер два. return'ы, спрятанные внутри условных скобок, компилятор не видел и из-за этого вывод метода пришлось заводить в отдельную переменную. Else'ы убрал, рассчет остатка от деления на 10 вынес в переменную reminder

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,33 +15,37 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 22;
+        int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 
-        if ((age < 0) || (age > 122)) {
-            return  "Некорректный ввод";
-        } else {
+        int remainder = age%10;
+        String output = "";
 
-            if ((age%10 == 0) || (age >= 5 && age <= 20) || (age%10 >= 5 && age%10 <= 9)) {
-                return "Вам " + age + " лет ";
-            } else {
-
-                if ((age % 10 == 1) & (age != 11)) {
-                    return "Вам " + age + " год ";
-                } else {
-
-                    if ((age % 10 == 2) ^ (age % 10 == 3) ^ (age % 10 == 4)) {
-                        return "Вам " + age + " года ";
-                    } else {
-                        return null;
-                    }
-                }
-            }
+        if ((age < 0) || (age > 127)) {
+            output = "Некорректный ввод";
         }
+
+        if ((remainder == 0) || (age >= 5 && age <= 20) ||(age >= 105 && age <= 120) || (remainder >= 5 && remainder <= 9) ) {
+            output ="Вам " + age + " лет";
+        }
+
+        if ((remainder == 1) & (age != 11) & (age != 111)) {
+            output ="Вам " + age + " год";
+        }
+
+        if ( ((age > 20 && age < 105) || age<5 || age>120 )&&((remainder == 2) ^ (remainder == 3) ^ (remainder == 4))) {
+            output ="Вам " + age + " года";
+        }
+
+        return output;
+
+
+
+
     }
 
 
