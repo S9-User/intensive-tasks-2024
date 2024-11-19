946fcad02733078c74dc152f30c1f1872fbae34c
S9-User <propoziti@yandex.ru>
2024-11-19 12:13:15 +0300

    Игорёха - S9-User - Task3 Попытка вторая. Да, действительно, всё можно гораздо проще.

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,20 +29,7 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if (year%400 == 0 && year%4 == 0){
-            return  true;
-        }
-        else {
-        if (year%100 == 0 && year%4 == 0){
-            return false;
-        }
-        else {
-        if (year%4 == 0){
-            return  true;
-        }
-        else {return false;
-        }
-        }
-        }
+        return ((year % 4 == 0) && (!(year % 100 == 0)) || (year % 400 == 0));
     }
 }
+
