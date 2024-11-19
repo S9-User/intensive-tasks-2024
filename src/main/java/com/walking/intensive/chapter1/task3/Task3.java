f910206a5cab8fabba0f6a4ffd448e6da4f79a1d
S9-User <propoziti@yandex.ru>
2024-11-19 10:40:23 +0300

    Игорёха - S9-User - Task3 Попытка первая. Цепочка else-ов замыкает множество возможных значений, страхуя от возможных ошибок. Тест код прошёл)

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,8 +29,20 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        if (year%400 == 0 && year%4 == 0){
+            return  true;
+        }
+        else {
+        if (year%100 == 0 && year%4 == 0){
+            return false;
+        }
+        else {
+        if (year%4 == 0){
+            return  true;
+        }
+        else {return false;
+        }
+        }
+        }
     }
 }
